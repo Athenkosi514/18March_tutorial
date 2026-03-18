7e2f235618af377c0e142d2255cf2e1c50a119f6
Athenkosi514 <silingaathenkosi7@gmail.com>
2026-03-18 10:24:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+
+
+
+    
+// document.getElementById("submit").addEventListener("click",function(event){
+    // let input = document.getElementById("input").value ;
+    // //let btn = document.getElementById("submit") ;
+    // let list = document.getElementById("list") ;
+//     let listItem = document.createElement("li");
+//     if(input ==""){
+//       alert("Do not  empty spaces !!");
+//     }
+//    listItem.textContent = input;
+
+//    list.appendChild(listItem);
+//    document.getElementById("input").value ="";
+   
+
+
+// })
+
+document.getElementById("submit").addEventListener("click" , function(event){
+
+    let task = document.getElementById("input").value ;
+    
+    let list = document.getElementById("list") ;
+    if(task ==""){
+              alert("Enter a task !!");
+              return;
+            }
+
+
+          // creating list item
+            let listItem = document.createElement("li");
+            listItem.textContent = task;
+            // creating delete button
+            let DeletBTN = document.createElement("button");
+            DeletBTN.textContent= "Delete";
+            // creating Toggle button / Mark as done button
+            let Toggle= document.createElement("button");
+            Toggle.textContent= "Mark as done";
+            
+            // appendibng delete button
+            listItem.appendChild(DeletBTN);
+            // appending Toggle
+            listItem.appendChild(Toggle);
+            list.appendChild(listItem);
+            // adding functionality to delete button
+            DeletBTN.addEventListener("click", function(){
+
+
+                list.removeChild(listItem)
+            })
+             // adding functionality to Toggle button
+            Toggle.addEventListener("click", function(){
+
+                listItem.style.textDecoration= "line-through";
+            })
+
+            document.getElementById("task").value="";
+
+
+})
